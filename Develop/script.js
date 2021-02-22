6fff959093f5bb9a048916e835c8774a357095d8
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-22 15:20:50 -0800

    working on var on js

--- Develop/script.js
+++ Develop/script.js
@@ -31,12 +31,44 @@ tenHr.val(localStorage.getItem("10am"))
 elevenHr.val(localStorage.getItem("11am"))
 twelveHr.val(localStorage.getItem("12pm"))
 thirteenHr.val(localStorage.getItem("1pm"))
-fourteenHr.val(localStorage.getItem("9am"))
-fifteenHr.val(localStorage.getItem("9am"))
-sixteenHr.val(localStorage.getItem("9am"))
-seventeenHr.val(localStorage.getItem(""))
+fourteenHr.val(localStorage.getItem("2pm"))
+fifteenHr.val(localStorage.getItem("3pm"))
+sixteenHr.val(localStorage.getItem("4pm"))
+seventeenHr.val(localStorage.getItem("5pm"))
 
 
 saveNine.on("click", () => {
     localStorage.setItem("9am", nineHr.val())
+})
+
+saveTen.on("click", () => {
+    localStorage.setItem("10am", tenHr.val())
+})
+
+saveEleven.on("click", () => {
+    localStorage.setItem("11am", elevenHr.val())
+})
+
+saveTwelve.on("click", () => {
+    localStorage.setItem("12pm", twelveHr.val())
+})
+
+saveThirteen.on("click", () => {
+    localStorage.setItem("1pm", thirteenHr.val())
+})
+
+saveFourteen.on("click", () => {
+    localStorage.setItem("2pm", fourteenHr.val())
+})
+
+saveFifteen.on("click", () => {
+    localStorage.setItem("3pm", fifteenHr.val())
+})
+
+saveSixteen.on("click", () => {
+    localStorage.setItem("4pm", sixteenHr.val())
+})
+
+saveSeventeen.on("click", () => {
+    localStorage.setItem("5pm", seventeenHr.val())
 })
